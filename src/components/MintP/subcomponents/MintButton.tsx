54083ac425919b5e54fa6f0ace388c51efa4524e
Alex Griciuc <agriciuc@gmail.com>
2022-05-17 12:10:49 +0300

    text changes

--- src/components/MintP/subcomponents/MintButton.tsx
+++ src/components/MintP/subcomponents/MintButton.tsx
@@ -70,7 +70,7 @@ export const MintButton = ({
       candyMachine?.state.isPresale ||
       candyMachine?.state.isWhitelistOnly
     ) {
-      return "WHITELIST MINT";
+      return "PRESALE MINT";
     } else if (cannotMintBecauseNotOnWhitelist) {
       return "WALLET NOT WHITELISTED";
     } else if (saleEnded) {
